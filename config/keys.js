0dc7e35f56da0c906f764d82988d30940f148e09
Bryant Vail <Bryanttv@outlook.com>
2018-06-10 16:53:55 -0400

    done

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//add this file to gitignore
+module.exports = {
+    google:{
+        clientSecret:   "591V6vssm-UXGOY07l9VYPn8",
+        clientID:       "708740628285-rf4vefeupm01jqgseesafj23gab1vdtr.apps.googleusercontent.com"
+    }
+    
+};
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,13 +2,15 @@
 
 const passport      = require("passport");
 const GoogleStrategy= require("passport-google-oauth20");
+const keys          = require("./keys");
+
 
 passport.use(
     new GoogleStrategy({//using the google api to sign in. 
         // options for the strategy
-        clientID    :"708740628285-rf4vefeupm01jqgseesafj23gab1vdtr.apps.googleusercontent.com",
-        clientSecret:"591V6vssm-UXGOY07l9VYPn8",
-        
+        clientID    :   keys.google.clientID,
+        clientSecret:   keys.google.clientSecret
+
 }), ()=> {
     //passport callback function
 });
