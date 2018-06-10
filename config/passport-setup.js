50a4563268628d6eb3c5a369fae963fe809f84d1
Bryant Vail <Bryanttv@outlook.com>
2018-06-10 16:42:33 -0400

    done here

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,8 +4,10 @@ const passport      = require("passport");
 const GoogleStrategy= require("passport-google-oauth20");
 
 passport.use(
-    new GoogleStrategy({
+    new GoogleStrategy({//using the google api to sign in. 
         // options for the strategy
+        clientID    :"708740628285-rf4vefeupm01jqgseesafj23gab1vdtr.apps.googleusercontent.com",
+        clientSecret:"591V6vssm-UXGOY07l9VYPn8",
         
 }), ()=> {
     //passport callback function
