de8bcf8c07b4e95abc4bc18f8ad015cdfc77ba39
Bryant Vail <Bryanttv@outlook.com>
2018-06-10 15:07:37 -0400

    ejs

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ const app       =   express();
 
 //set up view engine
 app.set("view engine", "ejs");
+
+//home route
+app.get("/", (req,res)=>{
+    res.render("home");
+});
+
+//start server command
+
+const port = /*findPort()*/3000;
+app.listen(port, ()=>{
+    console.log(
+        `app now listening for requests on port ${port}.`
+    );
+});//end app.listen()
