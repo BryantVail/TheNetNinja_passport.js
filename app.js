11d0951be9740f5790ce2eaebc4c1ac8f7ff7bcd
Bryant Vail <Bryanttv@outlook.com>
2018-06-15 16:09:20 -0400

    mongoDB connected

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ const authRoutes    =   require("./routes/auth-routes");
 
 //executing passport.use for strategies
 const passportSetup =   require("./config/passport-setup");
-
+//mongoose
+const mongoose      =   require("mongoose");
+const keys         =   require("./config/keys");
 
 //set up view engine
 app.set("view engine", "ejs");
+
+//connect to mongoDB
+mongoose.connect(keys.mlab.dbURI, ()=>{
+    console.log("Mongo Connected");
+});
+
+
 
 //set up routes
 app.use("/auth",authRoutes);//domain/auth/authRoutes
--- config/keys.js
+++ config/keys.js
@@ -3,6 +3,10 @@ module.exports = {
     google:{
         clientSecret:   "591V6vssm-UXGOY07l9VYPn8",
         clientID:       "708740628285-rf4vefeupm01jqgseesafj23gab1vdtr.apps.googleusercontent.com"
+    },
+    mlab:{
+        dbURI:"mongodb://bxvail:contrasena1@ds018248.mlab.com:18248/netninja_passport-tut"
+
     }
     
 };
